f232f1adbee2cfe73c2b2a1764597f732e3f9275
Yb99 <vaibhavv1904@gmail.com>
2024-06-14 12:00:35 +0530

    Basic Structure created

---
+++ stream-processor/src/test/java/com/yb/microservice/StreamProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yb.microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
